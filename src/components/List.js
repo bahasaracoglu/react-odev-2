41c118b777b6bbb3c8361bac55f3c12b35e9ecd3
bahasaracoglu <bahasaracoglu@gmail.com>
2024-01-04 20:12:50 +0300

    completed

--- src/components/List.js
+++ src/components/List.js
@@ -1,16 +1,20 @@
+import { useState } from "react";
+
 function List({ todoList, setTodoList, action }) {
+  const [editTodo, setEditTodo] = useState(null);
   const filteredList = todoList.filter((element) => {
     if (action === "active") {
       return !element.done;
     } else if (action === "completed") {
       return element.done;
     } else {
-      return true; // "All" seçeneği için tüm öğeleri göster
+      return true;
     }
   });
-  const handleToggle = (index) => {
+  const handleToggle = (tittle) => {
+    console.log("toggle tıklandı", tittle);
     const updatedList = todoList.map((element, i) => {
-      if (i === index) {
+      if (tittle === element.text) {
         return {
           ...element,
           done: !element.done,
@@ -21,34 +25,72 @@ function List({ todoList, setTodoList, action }) {
     setTodoList(updatedList);
   };
   const handleDelete = (index) => {
+    console.log("silme");
     setTodoList((prevList) => {
       return prevList.filter((element, i) => index !== i);
     });
   };
 
+  const handleEditClick = (e, element) => {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log("edit tıklandı", element);
+
+    if (editTodo) {
+      setEditTodo(null); // Eğer zaten düzenleme modundaysa, iptal et
+    } else {
+      setEditTodo(element); // Düzenleme modunu başlat
+    }
+    console.log(editTodo);
+  };
+  const handleEditChange = (e) => {
+    setEditTodo({
+      ...editTodo,
+      text: e.target.value,
+    });
+  };
+  const handleEditSubmit = () => {
+    const updatedList = todoList.map((element) =>
+      element.text === editTodo.text ? editTodo : element
+    );
+    setTodoList(updatedList);
+    setEditTodo(null); // Düzenleme modunu kapat
+  };
+
   return (
     <ul className="todo-list">
-      {filteredList.map((element, index) => {
-        return (
-          <li className="view" key={element.text}>
-            <div className="view">
-              <input
-                className="toggle"
-                type="checkbox"
-                checked={element.done}
-                onChange={() => handleToggle(index)}
-              />
-              <label className={element.done && "todo-done"}>
-                {element.text}
-              </label>
-              <button
-                className="destroy"
-                onClick={() => handleDelete(index)}
-              ></button>
-            </div>
-          </li>
-        );
-      })}
+      {filteredList.map((element, index) => (
+        <li className="view" key={element.text}>
+          <div className="view">
+            <input
+              className="toggle"
+              type="checkbox"
+              checked={element.done}
+              onChange={() => handleToggle(element.text)}
+            />
+            <label
+              onClick={(e) => handleEditClick(e, element)}
+              className={element.done ? "todo-done" : ""}
+            >
+              {editTodo && editTodo.text === element.text ? (
+                <input
+                  className="edit"
+                  value={editTodo.text}
+                  onChange={handleEditChange}
+                  onBlur={handleEditSubmit}
+                  autoFocus
+                />
+              ) : (
+                element.text
+              )}
+            </label>
+            <button
+              className="destroy"
+              onClick={() => handleDelete(index)}
+            ></button>
+          </div>
+        </li>
+      ))}
     </ul>
   );
 }
